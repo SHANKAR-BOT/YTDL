2bb7c210128f053f35f758e4c4496cc6faf1b520
Kurniawan Satria <satganzdevs@gmail.com>
2025-03-24 19:48:34 +0000

    first commit

--- public/utils.js
+++ public/utils.js
@@ -1,11 +1,5 @@
-const express = require("express")
 const axios = require("axios")
 const qs = require("qs")
-const fs = require('fs')
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 const searchVideo = async (url) => {
 let { data } = await axios.post("https://ssvid.net/api/ajax/search", qs.stringify({ query: url, vt: "home" }), {
@@ -20,7 +14,7 @@ headers: {
 return data
 }
 
-const ymp3 = async (url) => {
+exports.ymp3 = async (url) => {
 let res = await searchVideo(url)
 let { data } = await axios.post("https://ssvid.net/api/ajax/convert", qs.stringify({ vid: res.vid, k: res.links.mp3.mp3128.k }), {
 headers: {
@@ -34,7 +28,7 @@ headers: {
 return data
 }
 
-const ytmp4 = async (url) => {
+exports.ytmp4 = async (url) => {
 let res = await searchVideo(url)
 let { data } = await axios.post("https://ssvid.net/api/ajax/convert", qs.stringify({ vid: res.vid, k: res.links.mp4.auto.k }), {
 headers: {
@@ -48,31 +42,4 @@ headers: {
 return data
 }
 
-app.get("/", (req, res) => {
-res.sendFile('index.html', { root: __dirname + '/public' })
-})
 
-app.get("/download", async (req, res) => {
-let { url, format } = req.query
-if (!url) return res.status(400).json("URL tidak boleh kosong")
-try {
-let result = format === "mp3" ? await ymp3(url) : await ytmp4(url)
-let filename = `SatzzDev - ${result.title}.${format}`
-let file = fs.createWriteStream(filename)
-axios.get(result.dlink, { responseType: "stream" }).then(response => {
-response.data.pipe(file)
-file.on("finish", () => {
-res.download(filename, err => {
-if (!err) fs.unlinkSync(filename)
-})
-})
-}).catch(err => {
-console.error("Gagal download:", err)
-res.status(500).json("Gagal mengunduh file")
-})
-} catch (err) {
-res.status(500).json("Gagal mengambil data")
-}
-})
-
-app.listen(3000, () => console.log("Server berjalan di port 3000"))
